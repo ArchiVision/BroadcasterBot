dadd0e34763b84b8f01d100d5a14c01c898d08fa
overpathz <overpathz@gmail.com>
2023-04-01 16:03:54 +0300

    [BRC-12] Make bot start not in Application class

--- src/main/java/com/archivision/broadcaster/bot/BotStarter.java
+++ src/main/java/com/archivision/broadcaster/bot/BotStarter.java
@@ -1,16 +1,20 @@
 package com.archivision.broadcaster.bot;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 @RequiredArgsConstructor
+@Primary
 public class BotStarter {
     private final BotRegistrar botRegistrar;
     private final BroadcasterBot broadcasterBot;
     private final BotUpdateHandler botUpdateHandler;
 
+    @PostConstruct
     public void start() throws TelegramApiException {
         broadcasterBot.setUpdateHandler(botUpdateHandler);
         botRegistrar.register(broadcasterBot);
